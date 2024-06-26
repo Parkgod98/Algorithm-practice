2065384f25ef9bf988c5e1c5f197668e734083e0
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2024-06-26 12:43:57 +0900

    Update 피보나치 함수.py

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
@@ -1,11 +1,14 @@
 import sys
 
-zero = [0 for i in range(41)]
-one = [0 for i in range(41)]
-zero[0] = 1
+zero = [0 for i in range(41)] # 0이 몇번 호출됐는지 담을 배열.
+one = [0 for i in range(41)] # 1이 몇번 호출됐는지 담을 배열
+# 초기값 배정.
+zero[0] = 1 
 zero[1] = 0
 one[0] = 0
 one[1] = 1
+
+# 문제 조건상 가능한 모든 경우에 대한 0,1 호출 횟수 배열 생성.
 for i in range(2,41) :
     zero[i] = zero[i-1] +zero[i-2]
     one[i] = one[i-1] + one[i-2]
