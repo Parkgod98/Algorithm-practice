98af1f8ee36471a9466cfa4fa6bd6f4612a22ca5
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2024-06-27 10:14:06 +0900

    조합을 이용해서 해결

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,19 +1,26 @@
 import sys
-
-def dfs(m,stack,visited) :
-    if len(stack) == m :
-        print(' '.join(map(str,stack)))
-        return
-    for i in range(1,n+1) :
-        if visited[i] == False :
-            visited[i] = True
-            stack.append(i)
-            dfs(m,stack,visited)
-            stack.pop()
-            visited[i] = False
-
+# 조합 논리. 그러나 중복을 허용하는 조합. nCm
 n,m = map(int,sys.stdin.readline().split())
 
-visited =[False for i in range(n+1)]
-stack =[]
-dfs(m,stack,visited)
+base = [i for i in range(1,n+1)] # 조합을 수행할 배열
+example =[]
+
+# 배열 내 요소 띄어쓰기 출력.
+def PrintList(arr):
+    for i in arr :
+        print(i ,end =' ')
+    print("")
+# 조합 함수. 그러나 중복을 허용하는 조합. 재귀함수를 이용.
+def Comb(base,start,n,m) :
+    # m개를 고르고나면 프린트.
+    if len(example) == m :
+        PrintList(example)
+        return
+    for i in range(start+1,n) :
+        # 같은 값이 아닐때만 고른다. 같은값은 이미 고른값이니까.
+        if base[i] not in example :
+            example.append(base[i])
+            Comb(base,start,n,m)
+            example.pop()
+    return
+Comb(base,-1,n,m)
