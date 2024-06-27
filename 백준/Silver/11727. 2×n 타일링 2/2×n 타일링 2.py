a4c802cd250fe69d8375bc8034fcb6fb654e2986
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2024-06-27 11:40:29 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 44 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
@@ -1,12 +1,11 @@
 import sys
-import math
 
-rec = [0,1,3,5,11,21]
-
+case = [0,1,3,5]
 n = int(input())
-for i in range(6,n+1) :
+
+for i in range(4,n+1) :
     if i%2 == 0 :
-        rec.append(4*rec[i-2]-1)
-    else :
-        rec.append(4*rec[i-2]+1)
-print(rec[n]%10007)
+        case.append(case[i-1]*2+1)
+    else:
+        case.append(case[i - 1] * 2 - 1)
+print(case[n]%10007)
